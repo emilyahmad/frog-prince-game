18c727e7dd93150c91640ef991e2b7b08c4b69c3
Emily <emilyahmad77@gmail.com>
2026-04-29 13:53:26 -0400

    apparently skewing is messing w player movement, birds go different times now, done adjusting clouds

--- scripts/bird.gd
+++ scripts/bird.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 var cycle = false
+var randAni
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	randAni = randi() % 1 + .25
 	_hover()
 
 #func _physics_process(delta):
@@ -12,6 +14,7 @@ func _ready() -> void:
 		#cycle = true
 
 func _hover():
+	await get_tree().create_timer(randAni).timeout
 	var tween = create_tween()
 	tween.set_loops()
 	tween.tween_property(self, "global_position", global_position + Vector2(0, -100), 1)
